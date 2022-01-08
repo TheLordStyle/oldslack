a71e4ac12385b6a253b85c3d3da9cfd1daa86568
TheLordStyle <74387709+TheLordStyle@users.noreply.github.com>
2022-01-08 19:36:24 +0000

    Updated !staging

--- oldslack.py
+++ oldslack.py
@@ -21,7 +21,7 @@ class OldSlack(commands.Cog):
     async def staging(self, ctx):
         await ctx.trigger_typing()
         staging = "Current Staging is TBD we are moving south." \
-                  " Please move doctrine ships to Barleguet Baguette Bakery for moing down south.\n" \
+                  " Please move doctrine ships to Barleguet Baguette Bakery for moving down south.\n" \
                   "None doctrine ships should be moved to highsec for moving in the future.\n"
         staging += "Last updated 8th Jan 2022\n"
         return await ctx.send(staging)
